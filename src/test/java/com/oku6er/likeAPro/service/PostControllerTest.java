a8cb7f96387e5028cb8f2e10fbee7f6f2c78ef6c
Konstantin Petrov <k.petrov@petrel.ai>
2019-08-19 20:33:18 +0600

    Create mock postService and test class

---
+++ src/test/java/com/oku6er/likeAPro/service/PostControllerTest.java
@@ -0,0 +1,19 @@
+package com.oku6er.likeAPro.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+}
