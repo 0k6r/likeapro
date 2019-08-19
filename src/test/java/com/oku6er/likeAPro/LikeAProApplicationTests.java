457f9e6bffd120e4a0b164138c49d878a2445791
Konstantin Petrov <k.petrov@petrel.ai>
2019-08-19 20:15:07 +0600

    Initialize project

---
+++ src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oku6er.likeAPro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LikeAProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
