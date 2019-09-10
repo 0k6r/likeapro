3a39121256791e596baf313436ccc48ef3d3952c
Konstantin Petrov <k.petrov@petrel.ai>
2019-09-10 12:27:50 +0600

    Add save tag response implementation

--- src/main/java/com/oku6er/likeAPro/controller/TagController.java
+++ src/main/java/com/oku6er/likeAPro/controller/TagController.java
@@ -3,10 +3,11 @@ package com.oku6er.likeAPro.controller;
 import com.oku6er.likeAPro.model.Tag;
 import com.oku6er.likeAPro.service.ITagService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +20,10 @@ public class TagController {
     @GetMapping
     public ResponseEntity<List<Tag>> getAll() {
         return ResponseEntity.ok(tagService.findAll());
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Tag> save(@Validated @RequestBody Tag tag) {
+        return new ResponseEntity<>(tagService.save(tag), HttpStatus.CREATED);
     }
 }
