2fd37db919cd4d1c39a56e5945ca86e05a5b13a1
Konstantin Petrov <k.petrov@petrel.ai>
2019-09-18 23:32:36 +0600

    Add repository annotations

--- src/main/java/com/oku6er/likeAPro/repository/CommentRepository.java
+++ src/main/java/com/oku6er/likeAPro/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.oku6er.likeAPro.repository;
 
 import com.oku6er.likeAPro.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- src/main/java/com/oku6er/likeAPro/repository/TagRepository.java
+++ src/main/java/com/oku6er/likeAPro/repository/TagRepository.java
@@ -2,6 +2,8 @@ package com.oku6er.likeAPro.repository;
 
 import com.oku6er.likeAPro.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
