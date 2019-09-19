f86c7a322731f3aaa9a575b8d1ee8e7666cf9133
Konstantin Petrov <k.petrov@petrel.ai>
2019-09-19 14:52:37 +0600

    Remove testcontainers
    Need pull back after find best way

--- src/main/java/com/oku6er/likeAPro/LikeAProApplication.java
+++ src/main/java/com/oku6er/likeAPro/LikeAProApplication.java
@@ -3,12 +3,9 @@ package com.oku6er.likeAPro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-//@EnableJpaRepositories({"com.oku6er.likeAPro.repository"})
 @EnableTransactionManagement
 public class LikeAProApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
