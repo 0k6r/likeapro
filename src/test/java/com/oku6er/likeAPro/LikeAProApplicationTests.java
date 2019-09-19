45a7b5033c9c509609ae05ff1572d97f05089aea
Konstantin Petrov <k.petrov@petrel.ai>
2019-09-19 15:41:30 +0600

    Disable flyway

--- src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
+++ src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @EnableAutoConfiguration
+@Profile("test")
 class LikeAProApplicationTests {
 
     @Test
