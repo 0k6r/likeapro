5c00df0c9eaa7c786d868b57937825e3d7708a94
Konstantin Petrov <k.petrov@petrel.ai>
2019-09-19 15:36:21 +0600

    Enable auto configurations

--- src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
+++ src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
@@ -2,14 +2,13 @@ package com.oku6er.likeAPro;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(properties = {
-        "spring.flyway.enabled=false"
-})
-//@EnableAutoConfiguration
+@SpringBootTest
+@EnableAutoConfiguration
 class LikeAProApplicationTests {
 
     @Test
