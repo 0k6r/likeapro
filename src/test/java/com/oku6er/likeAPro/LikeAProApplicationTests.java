8b725db5398d5a8f102e2603623b4f2666e84946
Konstantin Petrov <k.petrov@petrel.ai>
2019-10-14 00:44:27 +0600

    Test travis with h2 database

--- src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
+++ src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @EnableAutoConfiguration
-@ActiveProfiles("test")
 class LikeAProApplicationTests {
 
     @Test
--- src/test/java/com/oku6er/likeAPro/controller/PostControllerTest.java
+++ src/test/java/com/oku6er/likeAPro/controller/PostControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,7 +31,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(PostController.class)
-@ActiveProfiles("test")
 public class PostControllerTest {
 
     @Autowired
