9b77f3bb728119fcd70e02bb91a645bf59ae5d84
Konstantin Petrov <k.petrov@petrel.ai>
2019-10-14 11:56:06 +0600

    Add test-source annotation in base junit class

--- src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
+++ src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@EnableAutoConfiguration
-@AutoConfigureTestDatabase
+@TestPropertySource("classpath:application-test.yml")
+//@EnableAutoConfiguration
+//@AutoConfigureTestDatabase
 class LikeAProApplicationTests {
 
     @Test
