69533aed9b39e01652d607ee9c60e25714ffbadb
Konstantin Petrov <k.petrov@petrel.ai>
2019-10-14 11:38:57 +0600

    Add config in base junit test class

--- src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
+++ src/test/java/com/oku6er/likeAPro/LikeAProApplicationTests.java
@@ -3,12 +3,16 @@ package com.oku6er.likeAPro;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-//@EnableAutoConfiguration
+@EnableAutoConfiguration
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class LikeAProApplicationTests {
 
     @Test
