c071a472e51030b69165ff9722bf3ece26e5cb6f
Konstantin Petrov <k.petrov@petrel.ai>
2019-10-15 22:10:28 +0600

    Test service in ci without qualifier

--- src/integrationTest/groovy/com/oku6er/likeAPro/service/PostITSpec.groovy
+++ src/integrationTest/groovy/com/oku6er/likeAPro/service/PostITSpec.groovy
@@ -7,7 +7,6 @@ import com.oku6er.likeAPro.model.post.Post
 import com.oku6er.likeAPro.model.post.Text
 import com.oku6er.likeAPro.service.post.IPostService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.ComponentScan
@@ -19,18 +18,16 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 @ComponentScan(basePackages = 'com.oku6er.likeAPro.service')
 class PostITSpec extends BaseIntegrationTest {
 
-    @Autowired
-    @Qualifier('PostService')
-    IPostService postService
+    @Autowired IPostService postService
 
     def 'save new post'() {
         given: 'post'
             def post = new Post().setTitle('Java').setSlug('java').setText(new Text()).setVote(10)
                 .setLanguage(Language.EN)
             post.addTag(new Tag('Java'))
-        when: 'save post'
+        when: 'save new post'
             def savedPost = postService.save(post)
-        then: 'post saved'
+        then: 'post saved and id cannot be null'
             savedPost.id != null
             savedPost.title == 'Java'
     }
--- src/main/java/com/oku6er/likeAPro/service/post/PostService.java
+++ src/main/java/com/oku6er/likeAPro/service/post/PostService.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @AllArgsConstructor
-@Service("PostService")
+@Service
 @Transactional
 public class PostService implements IPostService {
 
