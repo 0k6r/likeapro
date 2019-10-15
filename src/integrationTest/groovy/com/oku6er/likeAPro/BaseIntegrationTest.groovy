d6b1578030c044b00dd34daa497e2128a2927b98
Konstantin Petrov <k.petrov@petrel.ai>
2019-10-15 21:24:49 +0600

    Add spring profiles

--- src/integrationTest/groovy/com/oku6er/likeAPro/BaseIntegrationTest.groovy
+++ src/integrationTest/groovy/com/oku6er/likeAPro/BaseIntegrationTest.groovy
@@ -1,12 +1,13 @@
 package com.oku6er.likeAPro
 
-
+import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.spock.Testcontainers
 import spock.lang.Shared
 import spock.lang.Specification
 
 @Testcontainers
+@ActiveProfiles('integration-test')
 class BaseIntegrationTest extends Specification {
 
     @Shared
--- src/integrationTest/groovy/com/oku6er/likeAPro/service/PostTest.groovy
+++ src/integrationTest/groovy/com/oku6er/likeAPro/service/PostTest.groovy
@@ -5,22 +5,33 @@ import com.oku6er.likeAPro.model.Language
 import com.oku6er.likeAPro.model.post.Post
 import com.oku6er.likeAPro.model.post.Text
 import com.oku6er.likeAPro.service.post.IPostService
+import com.oku6er.likeAPro.service.tag.ITagService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = NONE)
 @ComponentScan(basePackages = 'com.oku6er.likeAPro.service')
+@ActiveProfiles(["integration-test"])
+@TestPropertySource('classpath:application-test.yml')
 class PostTest extends BaseIntegrationTest {
 
     @Autowired
     @Qualifier('PostService')
     IPostService postService
+
+    @Autowired ITagService tagService;
+
+    @Autowired
+    TestEntityManager entityManager;
 
     def 'save new post'() {
         given: 'post'
@@ -32,5 +43,6 @@ class PostTest extends BaseIntegrationTest {
             savedPost.id != null
             savedPost.title == 'Java'
             savedPost.slug == 'java'
+        System.err.println(tagService.findAll())
     }
 }
